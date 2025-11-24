7ba8ebee47e8d0249ef2c781fd6c4ba908c99d4c
Ociel <consultoria@winetpc.com>
2025-11-24 11:40:10 -0600

    Guardando cambios locales en main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -37,7 +37,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
             NavBar::begin([
                 'brandLabel' => Yii::$app->name,
                 'brandUrl' => Yii::$app->homeUrl,
-                'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
+                'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark bg-primary fixed-top']
             ]);
             echo Nav::widget([
                 'options' => ['class' => 'navbar-nav'],
