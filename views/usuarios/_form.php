d4833d627e7b2bf1eda2f551151f3879b57ae5ef
arturo <helpdesk.sistemas@winetpc.com>
2025-11-26 13:47:26 -0600

    cambios al formulario usuarios

--- views/usuarios/_form.php
+++ views/usuarios/_form.php
@@ -16,7 +16,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'password_hash')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'password_reset_token')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'password_reset_token')->hiddenInput(['maxlength' => true])->label(false) ?>
 
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
